eafdead39b0cb946cd40b12acb0b429ad3ef29de
Aayush Anand <aayusha23@iitk.ac.in>
2025-05-27 15:19:10 +0530

    sat solver docs added

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Apni Documentation 
+              Head over the Docs
           </Link>
         </div>
       </div>
